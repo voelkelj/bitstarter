9f823f6eb51124d1cf6f34f660e432a707aad8d7
Julian Voelkel <julian_voelkel@gmx.de>
2013-07-12 20:02:15 +0000

    Updated web.js once again

--- web.js
+++ web.js
@@ -1,20 +1,19 @@
 var express = require('express');
-
+var fs = require('fs'); 
 var app = express.createServer(express.logger());
 
-var http = require('http');
-var fs = require('fs'); 
-
-var indexFileBuffer = fs.readFileSync('./index.html');
-indexFileBuffer.write(indexFile);
-
+var index = fs.readFileSync("index.html");
+var buf = new Buffer(index);
 
 app.get('/', function(request, response) {
-//  response.send(indexFile);
-indexFileBuffer.toString(); 
+  response.send(buffer.toString());
+
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
